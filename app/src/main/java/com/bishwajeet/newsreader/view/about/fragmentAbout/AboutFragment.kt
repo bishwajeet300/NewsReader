341ad34fef78c8d4f94785ddc736520898b999bf
Bishwajeet Kumar <bishwajeet.k@loginextsolutions.com>
2020-03-11 20:25:26 +0530

    Added Navigation Components

---
+++ app/src/main/java/com/bishwajeet/newsreader/view/about/fragmentAbout/AboutFragment.kt
@@ -0,0 +1,24 @@
+package com.bishwajeet.newsreader.view.about.fragmentAbout
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.bishwajeet.newsreader.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class AboutFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+}
